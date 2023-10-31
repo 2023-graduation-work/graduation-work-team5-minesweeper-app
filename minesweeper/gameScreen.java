829079015dbffb21bb353a824be398d6ba1c1907
Reia <r.sekiguchi.sys22@morijyobi.ac.jp>
2023-10-31 10:42:10 +0900

    ゲーム画面の作成

--- minesweeper/gameScreen.java
+++ minesweeper/gameScreen.java
@@ -8,14 +8,39 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class gameScreen extends World
 {
-
+    private int timecount = 0;
     /**
      * Constructor for objects of class gameScreen.
      * 
      */
     public gameScreen()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 550, 1);
+        drawCheckerPattern();
+    }
+     private void drawCheckerPattern() {
+        int cellSize = 30; // 1つのチェックマスのサイズ
+        int numColumns = 18; // 列数
+        int numRows = 14; // 行数
+        Color[] colors = {new Color(192, 192, 192), new Color(211, 211, 211)}; // チェック柄の色
+        // チェック柄を中央に配置
+        int centerX = getWidth() / 2 - (numColumns * cellSize) / 2;
+        int centerY = getHeight() / 2 - (numRows * cellSize) / 2;
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numColumns; col++) {
+                int colorIndex = (row + col) % 2; // チェック柄の色を交互に切り替え
+                Color cellColor = colors[colorIndex];
+                for (int x = centerX + col * cellSize; x < centerX + (col + 1) * cellSize; x++) {
+                    for (int y = centerY + row * cellSize; y < centerY + (row + 1) * cellSize; y++) {
+                        getBackground().setColorAt(x, y, cellColor);
+                    }
+                }
+            }
+        }
+    }
+    public void act() {
+       
+       timecount += 1;
+       showText("Time: " + timecount, 60, 30 );
     }
 }
